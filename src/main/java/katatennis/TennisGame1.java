b81675eded9b3703f1e073c0b4d1e3de3967f22c
Andreas Rey Malissein <andreasrey87@gmail.com>
2024-09-11 18:25:29 +0200

    Extract getScore method

--- src/main/java/katatennis/TennisGame1.java
+++ src/main/java/katatennis/TennisGame1.java
@@ -24,52 +24,75 @@ public class TennisGame1 implements TennisGame {
         int tempScore=0;
         if (player1Score == player2Score)
         {
-            switch (player1Score)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-
-            }
+            score = allAnalyse(player1Score);
         }
         else if (player1Score >=4 || player2Score >=4)
         {
-            int minusResult = player1Score - player2Score;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            score = advantageAnalyse(player1Score, player2Score);
         }
         else
         {
-            for (int i=1; i<3; i++)
+            score = regularAnalyse(player1Score, player2Score);
+        }
+        return score;
+    }
+
+    public String allAnalyse(int player1Score) {
+        String score;
+        switch (player1Score)
+        {
+            case 0:
+                    score = "Love-All";
+                break;
+            case 1:
+                    score = "Fifteen-All";
+                break;
+            case 2:
+                    score = "Thirty-All";
+                break;
+            default:
+                    score = "Deuce";
+                break;
+
+        }
+        return score;
+    }
+
+    public String advantageAnalyse(int player1Score, int player2Score) {
+        String score;
+        int minusResult = player1Score-player2Score;
+        if (minusResult==1)
+            score = "Advantage player1";
+        else if (minusResult ==-1)
+            score = "Advantage player2";
+        else if (minusResult>=2)
+            score = "Win for player1";
+        else
+            score = "Win for player2";
+        return score;
+    }
+
+    public String regularAnalyse(int player1Score, int player2Score){
+        String score = "";
+        int tempScore=0;
+        for (int i=1; i<3; i++)
+        {
+            if (i==1) tempScore = player1Score;
+            else { score+="-"; tempScore = player2Score;}
+            switch(tempScore)
             {
-                if (i==1) tempScore = player1Score;
-                else { score+="-"; tempScore = player2Score;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+                case 0:
+                    score+="Love";
+                    break;
+                case 1:
+                    score+="Fifteen";
+                    break;
+                case 2:
+                    score+="Thirty";
+                    break;
+                case 3:
+                    score+="Forty";
+                    break;
             }
         }
         return score;
